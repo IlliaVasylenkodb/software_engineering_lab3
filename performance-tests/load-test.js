74a05ac1ef99049a15ec57221210a9ddf75f93b6
Illia Vasylenko657 <155399547+IlliaVasylenkodb@users.noreply.github.com>
2026-04-08 19:17:01 +0300

    Update load-test.js

--- performance-tests/load-test.js
+++ performance-tests/load-test.js
@@ -1,1 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
 
+export const options = {
+  stages: [
+    { duration: '10s', target: 10 }, // Розгін
+    { duration: '20s', target: 10 }, // Стабільність
+    { duration: '10s', target: 0 },  // Завершення
+  ],
+};
+
+export default function () {
+  http.get('http://localhost:8080/');
+  sleep(1);
+}
